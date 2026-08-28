a8dc3b9d1a79bc4906cdb2699690056fc8cf2d91
shokirjon1902 <shokirjontojiboyev155@gmail.com>
2026-08-28 12:56:39 +0500

    feat: Jack Ma maslahatlari

--- train.js
+++ train.js
@@ -1,1 +1,32 @@
-console.log("TRAIN AREA");
+console.log("Jack Ma maslahatlari");
+const list = [
+  "yaxshi talaba buling ", //0-20
+  "togri boshliq tanlang va koproq xato qiling ", // 20-30
+  "uzingizga ishlashni boshlang ", //30-40
+  "siz kuchli bolgan narsalarni qiling", // 40-50
+  "yoshlarga invistetsiya qiling ", //50-60
+  "endi dam oling , fodasi yoq", // 60+
+];
+
+function maslahatBering(a, callback) {
+  if (typeof a !== "number") callback("inser a number ", "null");
+  else if (a <= 20) callback(null, list[0]);
+  else if (a > 20 && a <= 30) callback(null, list[1]);
+  else if (a > 30 && a <= 40) callback(null, list[2]);
+  else if (a > 40 && a <= 50) callback(null, list[3]);
+  else if (a > 50 && a <= 60) callback(null, list[4]);
+  else {
+    setTimeout(function () {
+      callback(null, list[5]);
+    }, 4000);
+  }
+}
+
+console.log("passed here 0");
+
+maslahatBering(70, (err, data) => {
+  if (err) console.log("ERROR:", err);
+  console.log("javob;", data);
+});
+
+console.log("passed here 1");
