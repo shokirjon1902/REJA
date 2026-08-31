5f4b5ec6a97743b8b899c28c56aa74d9bbc8c6fa
shokirjon1902 <shokirjontojiboyev155@gmail.com>
2026-08-31 19:07:41 +0500

    feat: CRUD operatsiyalari

--- app.js
+++ app.js
@@ -28,8 +28,18 @@ app.set("view engine", "ejs");
 
 // 4 Routing code
 app.post("/create-item", (req, res) => {
+  console.log("user entered /");
+
   console.log(req.body);
-  res.json({ test: "success" });
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("something went wrong");
+    } else {
+      res.end("succcessfully added");
+    }
+  });
 });
 
 app.get("/author", (req, res) => {
@@ -37,7 +47,17 @@ app.get("/author", (req, res) => {
 });
 
 app.get("/", function (req, res) {
-  res.render("reja");
+  console.log("user entered /");
+  db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        res.end("something went wrong");
+      } else {
+        res.render("reja", { items: data });
+      }
+    });
 });
 
 module.exports = app;
