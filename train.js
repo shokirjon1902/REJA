7aaf3c8646b5aa2c48ba2865fb91332a3b982649
shokirjon1902 <shokirjontojiboyev155@gmail.com>
2026-09-02 16:40:13 +0500

    completed TASK - C

--- train.js
+++ train.js
@@ -1,21 +1,69 @@
+//TASK - C
+/*
+Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+*/
+
+class Shop {
+  constructor(non, lagmon, cola) {
+    this.non = non;
+    this.lagmon = lagmon;
+    this.cola = cola;
+  }
+
+  qoldiq() {
+    let vaqt = new Date();
+    let soat = vaqt.getHours();
+    let minut = vaqt.getMinutes();
+
+    return `hozir ${soat}:${minut}da ${this.non}ta non, ${this.lagmon}ta lagmon, ${this.cola}ta cola mavjud ! `;
+  }
+
+  sotish(name, number) {
+    if (name === "non") {
+      this.non -= number;
+    }
+    if (name === "lagmon") {
+      this.lagmon -= number;
+    }
+    if (name === "cola") {
+      this.cola -= number;
+    }
+  }
+  qabul(name, number) {
+    if (name === "non") {
+      this.non += number;
+    }
+    if (name === "lagmon") {
+      this.lagmon += number;
+    }
+    if (name === "cola") this.cola += number;
+  }
+}
+const shop = new Shop(4, 5, 2);
+
+shop.sotish("non", 3);
+shop.qabul("cola", 2);
+console.log(shop.qoldiq());
+
 // TASK - B
 /*
 Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
 MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
 */
 
-function countDigits(str) {
-  let number = 0;
-  for (let a = 0; a < str.length; a++) {
-    if (str[a] >= "0" && str[a] <= "9") {
-      number++;
-    }
-  }
-  return number;
-}
+// function countDigits(str) {
+//   let number = 0;
+//   for (let a = 0; a < str.length; a++) {
+//     if (str[a] >= "0" && str[a] <= "9") {
+//       number++;
+//     }
+//   }
+//   return number;
+// }
 
-const answer = countDigits("a23d2a54fvfrdy79we11t0sf5gb9");
-console.log("Answer: ", answer);
+// const answer = countDigits("a23d2a54fvfrdy79we11t0sf5gb9");
+// console.log("Answer: ", answer);
 
 // TASK - A
 // Harf sifatida kiritilgan birinchi parametr,
