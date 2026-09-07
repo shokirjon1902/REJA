43d4b6a832d9e54f45c1424c0446d6872306356b
shokirjon1902 <shokirjontojiboyev155@gmail.com>
2026-09-07 17:58:16 +0500

    TASK D

--- train.js
+++ train.js
@@ -1,50 +1,72 @@
+/*
+TASK D : 
+
+Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
+
+MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
+*/
+
+function checkContent(str1, str2) {
+  if (str1.length === str2.length) {
+    for (let i = 0; i < str1; i++) {
+      if (str2.icludes(str1[i])) {
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+}
+
+console.log(checkContent("mitgroup", "gmtiprou"));
 //TASK - C
 /*
 Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
 MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
 */
 
-class Shop {
-  constructor(non, lagmon, cola) {
-    this.non = non;
-    this.lagmon = lagmon;
-    this.cola = cola;
-  }
+// class Shop {
+//   constructor(non, lagmon, cola) {
+//     this.non = non;
+//     this.lagmon = lagmon;
+//     this.cola = cola;
+//   }
 
-  qoldiq() {
-    let vaqt = new Date();
-    let soat = vaqt.getHours();
-    let minut = vaqt.getMinutes();
+//   qoldiq() {
+//     let vaqt = new Date();
+//     let soat = vaqt.getHours();
+//     let minut = vaqt.getMinutes();
 
-    return `hozir ${soat}:${minut}da ${this.non}ta non, ${this.lagmon}ta lagmon, ${this.cola}ta cola mavjud ! `;
-  }
+//     return `hozir ${soat}:${minut}da ${this.non}ta non, ${this.lagmon}ta lagmon, ${this.cola}ta cola mavjud ! `;
+//   }
 
-  sotish(name, number) {
-    if (name === "non") {
-      this.non -= number;
-    }
-    if (name === "lagmon") {
-      this.lagmon -= number;
-    }
-    if (name === "cola") {
-      this.cola -= number;
-    }
-  }
-  qabul(name, number) {
-    if (name === "non") {
-      this.non += number;
-    }
-    if (name === "lagmon") {
-      this.lagmon += number;
-    }
-    if (name === "cola") this.cola += number;
-  }
-}
-const shop = new Shop(4, 5, 2);
+//   sotish(name, number) {
+//     if (name === "non") {
+//       this.non -= number;
+//     }
+//     if (name === "lagmon") {
+//       this.lagmon -= number;
+//     }
+//     if (name === "cola") {
+//       this.cola -= number;
+//     }
+//   }
+//   qabul(name, number) {
+//     if (name === "non") {
+//       this.non += number;
+//     }
+//     if (name === "lagmon") {
+//       this.lagmon += number;
+//     }
+//     if (name === "cola") this.cola += number;
+//   }
+// }
+// const shop = new Shop(4, 5, 2);
 
-shop.sotish("non", 3);
-shop.qabul("cola", 2);
-console.log(shop.qoldiq());
+// shop.sotish("non", 3);
+// shop.qabul("cola", 2);
+// console.log(shop.qoldiq());
 
 // TASK - B
 /*
